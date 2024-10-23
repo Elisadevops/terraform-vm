627f6284dd678c130fe48af7f1ba25ce325209a1
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 18:16:09 -0400

    Script modified

--- main.tf
+++ main.tf
@@ -6,11 +6,10 @@ terraform {
     }
   }
 }
-
 provider "proxmox" {
   pm_api_url     = "https://64.255.204.91:8006/api2/json"  
   pm_user        = var.pm_user                                
   pm_password    = var.pm_password                            
   pm_tls_insecure = true  
-}                                   
+} 
 
