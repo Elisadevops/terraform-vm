48d27b4caef75e6411274e5bbc769680b8d8d7fc
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 16:11:45 -0400

    added the provider

--- main.tf
+++ main.tf
@@ -7,3 +7,9 @@ terraform {
   }
 }
 
+provider "proxmox" {
+  pm_api_url     = "https://64.255.204.91:8006/api2/json"  
+  pm_user        = "elisa@pve"              
+  pm_password     = "QODTXP3750DSuhCH58O3zOdkuVTyp9Nh"                          
+  pm_tls_insecure = true                                     
+}
