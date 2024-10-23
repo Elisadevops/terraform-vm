db8a40948573006633c22789c1a9135e9f5c04c9
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 17:28:42 -0400

    Changes made in the script

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "proxmox" {
   pm_api_url     = "https://64.255.204.91:8006/api2/json"  
-  pm_user        = "elisa@pve"              
-  pm_password     = "QODTXP3750DSuhCH58O3zOdkuVTyp9Nh"                          
+  pm_user        = var.pm_user                                
+  pm_password    = var.pm_password                            
   pm_tls_insecure = true                                     
-}
+
