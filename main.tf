a7731d329395dd50e897643cd544814fde387c1f
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 19:39:29 -0400

    del uneccesary

--- main.tf
+++ main.tf
@@ -13,3 +13,23 @@ provider "proxmox" {
   pm_tls_insecure = true  
 } 
 
+#Configure Proxmox Resources Here
+resource "proxmox_vm_qemu" "test_terraform" {
+  name        = "terraform-vm"
+  target_node = "goc2b1p3" 
+  clone       = "123"
+  os_type     = "cloud-init"
+  cores       = 2
+  memory      = 1024
+  scsihw = "virtio-scsi-pci"
+  disk {
+    size = "10G"
+    storage = "local-lvm"
+    type = "SCSI"
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"            # your network bridge name
+    tag    = 150                # VLAN ID 150
+  }
+}
