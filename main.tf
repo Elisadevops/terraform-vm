7e716430631b80d22fc733bf94fe06949cb60c7b
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 17:32:47 -0400

    Changes made

--- main.tf
+++ main.tf
@@ -11,5 +11,6 @@ provider "proxmox" {
   pm_api_url     = "https://64.255.204.91:8006/api2/json"  
   pm_user        = var.pm_user                                
   pm_password    = var.pm_password                            
-  pm_tls_insecure = true                                     
+  pm_tls_insecure = true  
+}                                   
 
