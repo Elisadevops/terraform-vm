ea00e98c269df923ec0e5e463727af7dbebfe9ff
elisadevops <elisa.yahweh@gmail.com>
2024-10-23 21:50:11 -0400

    error rectified

--- main.tf
+++ main.tf
@@ -7,17 +7,19 @@ terraform {
   }
 }
 provider "proxmox" {
-  pm_api_url     = "https://64.255.204.91:8006/api2/json"  # Proxmox server API endpoint
-  pm_user        = "elisa@pve"                             # Your Proxmox user with necessary permissions
-  pm_password    = "QODTXP3750DSuhCH58O3zOdkuVTyp9Nh"      # Your Proxmox password
-  pm_tls_insecure = true                                   # Allows insecure TLS connections (set false in production)
+  pm_api_url     = "https://64.255.204.91:8006/api2/json"  
+  pm_user        = "elisa@pve"                             
+  pm_password    = "QODTXP3750DSuhCH58O3zOdkuVTyp9Nh"      
+  pm_tls_insecure = true                                   
 }
 
 #Configure Proxmox Resources Here
-resource "proxmox_vm_qemu" "test_terraform" {
-  name        = "terraform-vm"
-  target_node = "goc2b1p3" 
-  clone       = "123"
+resource "proxmox_vm_qemu" "terraform_proxmox_test" {
+  name        = "terraform-vm-${count.index + 1}"  # VM Name with index
+  target_node = var.target_node                         # Proxmox node name
+  clone       = var.template_name                       # Template to clone
+  count       = 1  
+  vmid        =  0861
   os_type     = "cloud-init"
   cores       = 2
   memory      = 1024
@@ -29,6 +31,7 @@ resource "proxmox_vm_qemu" "test_terraform" {
   }
   network {
     model = "virtio"
-    bridge = "vmbr0"                         
+    bridge = "vmbr0" 
+    tag    = "151"                        
   }
 }
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "target_node" {
+    default = "goc2b1p3"
+}
+
+variable "template_name" {
+    default = "Elisa-cloud-init"
+}
